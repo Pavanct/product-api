3551755eb7502e537aec1e6414c3e4c4f2b8d027
Pavan Chitradurga Thammanna <pavanct143@gmail.com>
2019-03-15 02:23:15 +0100

    CRUD operations working

--- product/product.controller.js
+++ product/product.controller.js
@@ -1,73 +1,79 @@
-const Product = require('./product.model');
+const Product = require('./product.model.js');
 
 exports.create = (req, res) => {
     //request validation
-    if(!req.body){
+    console.log(req.body);
+    if (!req.body) {
         return res.status(400).send({
             message: "Product content could not be empty"
         });
     }
 
+    //Create of a Product 
+    const product = new Product({
+        title: req.body.title || "No product title",
+        description: req.body.description,
+        price: req.body.price,
+        company: req.body.company
+    });
 
-//Create of a Product 
-const product = new Product({
-    title: req.body.title || "No product title",
-    description: req.body.description,
-    price: req.body.price,
-    company: req.body.company
-});
-
-//Save products 
-product.save()
-.then(data => {
-    res.send(data);
-}).catch(err => {
-    res.status(500).send({
-        message: err.message || "Something wrong while creating the product"
-    })
-})
+    //Save products 
+    product.save()
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Something wrong while creating the product"
+            })
+        })
 };
 
 //Retrieve all products from the database
 exports.findAll = (req, res) => {
+    console.log("inside findAll");
+
     Product.find()
-    .then(products => {
-        Product.find().then(products=>{
-            console.log(products);
-        }).catch(err=>{
+        .then(products => {
+            if (products.length < 1) {
+                res.send({
+                    message: "No products found"
+                });
+            } else {
+                res.send(products);
+            }
+        }).catch(err => {
             res.status(500).send({
-                message: err.message || "Something wrong while retrieving the products"
+                message: err.message || "Something wrong while retrieving products."
             });
         });
-    });
-}
+};
 
 //Find a single product with product id
 exports.findOne = (req, res) => {
     Product.findById(req.params.productId)
-    .then(product => {
-        if(!product){
-            return res.status(400).send({
-                message: "Product not found with id" + req.params.productId
+        .then(product => {
+            if (!product) {
+                return res.status(400).send({
+                    message: "Product not found with id" + req.params.productId
+                });
+            }
+            res.send(product);
+        }).catch(err => {
+            if (err.kind == 'ObjectId') {
+                return res.status(404).send({
+                    message: "Something wrong with retrieving product with id " + req.params.productId
+                });
+            }
+            return res.status(500).send({
+                message: "Something wrong retrieving product with id " + req.params.productId
             });
-        }
-        res.send(product);
-    }).catch(err => {
-        if(err.kind == 'ObjectId'){
-            return res.status(404).send({
-                message: "Something wrong with retrieving product with id " + req.params.productId
-            });
-        }
-        return res.status(500).send({
-            message: "Something wrong retrieving product with id " + req.params.productId
         });
-    });
 };
 
 //Update a product
 exports.update = (req, res) => {
     //Validate request
-    if(!req.body){
+    if (!req.body) {
         return res.status(400).send({
             message: "Product content can not be body"
         });
@@ -78,16 +84,18 @@ exports.update = (req, res) => {
         title: req.body.title || "No product title",
         description: req.body.description,
         price: req.body.price,
-        company: req.body.company 
-    }, {new: true}).then(product => {
-        if(!product){
+        company: req.body.company
+    }, {
+        new: true
+    }).then(product => {
+        if (!product) {
             res.status(400).send({
                 message: "Product not found with id " + req.params.productId
             })
         }
         res.send(product);
     }).catch(err => {
-        if(err.kind == 'ObjectId'){
+        if (err.kind == 'ObjectId') {
             return res.status(404).send({
                 message: "Product not found with id " + req.params.productId
             });
@@ -102,14 +110,14 @@ exports.update = (req, res) => {
 // Delete a product with the specified productId in the request
 exports.delete = (req, res) => {
     Product.findByIdAndDelete(req.params.productId)
-    .then(product => {
-        if(!product){
-            return res.status(404).send({
-                message: "Product not found with id " + req.params.productId
+        .then(product => {
+            if (!product) {
+                return res.status(404).send({
+                    message: "Product not found with id " + req.params.productId
+                });
+            }
+            return res.status(200).send({
+                message: "Product successfully deleted " + req.params.productId
             });
-        }
-        return res.status(500).send({
-            message: "Product not found with id " + req.params.productId
         });
-    });
 };
--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
-const app = express();
-require('./product/product.routes.js')(app);
+const cors = require("cors");
+var app = express();
+
 //parse requests
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+// parse application/json
+//app.use(bodyParser.json());
+app.use(cors());
 //Enable CORS for all HTTP requests
-app.use(function(req, res, next){
+/*app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Content-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
-
+});*/
 //connecting to database
 const config = require("./config");
 const mongoose = require("mongoose");
 
+//import routes after body parse
+require('./product/product.routes.js')(app);
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.url, {
-    useNewUrlParser: true
-}).then(()=> {
+mongoose.connect(config.url, { useNewUrlParser: true })
+.then(()=> {
     console.log("Connected to database successfully")
 }).catch(err => {
     console.log("could not connect to database. Exiting now...", err);
