08a02840ae388ec942407727db7eae58ff799878
Pavan Chitradurga Thammanna <pavanct143@gmail.com>
2019-03-15 01:21:22 +0100

    enable cors

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+require('./product/product.routes.js')(app);
 //parse requests
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Enable CORS for all HTTP requests
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Content-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //connecting to database
 const config = require("./config");
 const mongoose = require("mongoose");
+
 
 mongoose.Promise = global.Promise;
 
